5f98dfd2cbe9e7118f99af7a79255b832153f610
jaeeun <ssse88@naver.com>
2017-11-16 20:32:33 -0800

    chatclient

--- chatClient.c
+++ chatClient.c
@@ -9,6 +9,7 @@
 #define IPADDR "0.0.0.0"
 
 char buffer[BUFSIZE];
+char sndBuffer[BUFSIZE], rcvBuffer[BUFSIZE];
 
 int main(int argc,char*argv[])
 {
@@ -20,6 +21,7 @@ int main(int argc,char*argv[])
    struct sockaddr_in c_addr;
    int len;
    int n;
+   int n_left,n_recv;
    char rcvBuffer[BUFSIZE];
    if(c_sock==-1)
    {
@@ -46,13 +48,44 @@ int main(int argc,char*argv[])
    buffer[strlen(buffer)]='\0';
    write(c_sock,buffer,strlen(buffer)+1);
 
-   // request the service and processing
-   if ((n=read(c_sock,rcvBuffer,sizeof(rcvBuffer)))<0)
+   //
+   while(1){
+   if((n=read(0,sndBuffer,BUFSIZE))>0)
    {
-      return -1;
-   }
-   rcvBuffer[n]='\0';
-   printf("received Data : %s\n",rcvBuffer);
 
-   close(c_socket);
+     sndBuffer[n]='\0';
+     if(!strcmp(sndBuffer,"quit\n")) break;
+  
+    printf("original data : %s", sndBuffer);
+    if((n=write(c_sock,sndBuffer,strlen(sndBuffer)))<0)
+    {
+     return (-1);
+    }
+
+
+    n_left=n;
+    n_recv=0;
+
+    while(n_left >0)
+    {
+      if((n=read(c_sock,&rcvBuffer[n_recv],n_left))<0)
+      {
+        return (-1);
+      }
+      n_left=n_left-n;
+      n_recv=n_recv+n;
+    }
+
+   rcvBuffer[n_recv]='\0';
+   printf("echod data : %s",rcvBuffer);
+  }
+   // request the service and processing
+  // if ((n=read(c_sock,rcvBuffer,sizeof(rcvBuffer)))<0)
+  // {
+  //    return (-1);
+  // }
+  // rcvBuffer[n]='\0';
+  // printf("received Data : %s\n",rcvBuffer);
+}
+   close(c_sock);
 }
