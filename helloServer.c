7e94c008839d971b2017bb488afefb9112291e60
sing815 <sing_815@naver.com>
2017-11-23 08:14:28 -0800

    helloServer.c answer

--- helloServer.c
+++ helloServer.c
@@ -17,31 +17,31 @@ void error(char *message)
 
 int main(int argc, char *argv[])
 {
-	int serv_sock, clnt_sock, str_len, i;
-	char message[BUFSIZE];
+	int serv_sock, clnt_sock, recv_len,status;
+	char message[BUFSIZE], PORT[5];
 
-	//char *serv_port = "9190";
+	char serv_message[] = "Welcom to chatting server!!\n";
 
-	struct sockaddr_in serv_addr, clnt_addr;
+        struct sockaddr_in serv_addr;
+        struct sockaddr_in clnt_addr;
+
 	socklen_t clnt_addr_size;
-
-	//char message[]="_Hello World!_";
-
-	if(argc!=2)
-	{
-		printf("Usage : %s <port> \n", argv[0]);
-		exit(1);
-	}
 	
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+
+	pid_t pid;
+
+	printf("Input Server Port Number : ");
+	fflush(stdin);
+	scanf("%s", PORT);
+	
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 	if(serv_sock == -1)
-		error("socket() error");
+		error("socket() error\n");
 
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family=AF_INET;
 	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-	//serv_addr.sin_port=htons(atoi(serv_port));
-         serv_addr.sin_port=htons(atoi(argv[1]));
+         serv_addr.sin_port=htons(atoi(PORT));
 
 
 	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1)
@@ -50,25 +50,40 @@ int main(int argc, char *argv[])
 	if(listen(serv_sock, 5)==-1)
 		error("listen() error");
 
-	clnt_addr_size=sizeof(clnt_addr);
+	clnt_addr_size = sizeof(clnt_addr);
 
-	for(i = 0 ; i < BUFSIZE; i++)
+	clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_sock, &clnt_addr_size);
+        if(clnt_sock == -1)
+                error("accept() error\n");
+	else
 	{
-		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-		if(clnt_sock==-1)
-			error("accept() error");
-		else
-			printf("connected client %d\n", i+1);
+		printf("connected Client\n");
+		printf("Chatting ON");
+		write(clnt_sock, serv_message, sizeof(serv_message));
+		pid = fork();
+	}
+	
+	while(1)
+	{
+		if(pid == 0)
+		{
+			recv_len = read(clnt_sock, message, BUFSIZE);
+	
+			if(recv_len > 1)
+			{
+				message[recv_len] = 0;
+				printf("message form Client : %s",message);
+			}
 
-		while((str_len = read(clnt_sock, message, BUFSIZE)) != 0)
-			write(clnt_sock, message, sizeof(str_len));
+
+		}
 		
-		close(clnt_sock);
+		
 	}
 
 
 	close(serv_sock);
-
+	close(clnt_sock);
 	return 0;
 
 }
