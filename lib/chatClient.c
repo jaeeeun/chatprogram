a68edff8fce1219396121ce905281c3606e65c7e
jaeeun <ssse88@naver.com>
2017-11-26 03:37:33 -0800

    client fork

--- lib/chatClient.c
+++ lib/chatClient.c
@@ -20,12 +20,15 @@ int main(int argc,char*argv[])
    int str_len,recv_len,recv_cnt;
    char message[BUFSIZE];
 
+   /* pid */
+   pid_t pid;
+
    /* server address,port */
    int serverPort;
-   char *serverAddr;
+   char *serverAddr="0.0.0.0";
   
-   printf("server address:");
-   scanf("%s",serverAddr);
+   //printf("server address:");
+   //scanf("%s",serverAddr);
    printf("server port:");
    scanf("%d",&serverPort);
 
@@ -50,32 +53,57 @@ int main(int argc,char*argv[])
    printf("---------------------------------------------\n");
    printf("\n");
   
+   pid=fork(); // make child process
+
+  
+   recv_len=read(c_sock,message,BUFSIZE);
+   message[recv_len]=0;
 
    while(1)
    {
       
-      fputs("Input message : ",stdout);
-      fgets(message, BUFSIZE, stdin);
-      printf("\n");
-
-      if(!strcmp(message,"Quit\n")||!strcmp(message,"Q\n"))
-         break;
-
-      str_len=write(c_sock,message,strlen(message));
-      recv_len=0;
-
-      while(recv_len<str_len)
+      if(pid==0)
       {
-          recv_cnt = read(c_sock,&message[recv_len],BUFSIZE);
-          if(recv_cnt ==-1)
-               printf("error");
-          recv_len+=recv_cnt;
+         if(recv_len>1)
+         {
+            recv_len=read(c_sock,message,BUFSIZE);
+            if(!strcmp(message,"Q ")||!strcmp(message,"q "))
+             break;
+            message[recv_len]=0;
+            printf("server:%s",message);
+         }
       }
+      else
+      {
+          printf("client:");
+          fgets(message,BUFSIZE,stdin);
+          if(!strcmp(message,"Q ")||!strcmp(message,"q"))
+          break;
+          write(c_sock,message,BUFSIZE);
+      }
+
+     // fputs("Input message : ",stdout);
+     // fgets(message, BUFSIZE, stdin);
+     // printf("\n");
+
+     // if(!strcmp(message,"Quit\n")||!strcmp(message,"Q\n"))
+     //    break;
+
+    //  str_len=write(c_sock,message,strlen(message));
+    //  recv_len=0;
+
+    //  while(recv_len<str_len)
+    //  {
+    //      recv_cnt = read(c_sock,&message[recv_len],BUFSIZE);
+    //      if(recv_cnt ==-1)
+    //           printf("error");
+    //      recv_len+=recv_cnt;
+    //  }
       
-      message[str_len]=0;
+    //  message[str_len]=0;
       
-      printf("Message from server : %s",message);
-      printf("\n");
+    //  printf("Message from server : %s",message);
+    //  printf("\n");
       
 
     
