19be9aa43058474ab064b10de969a0dc1caec830
jaeeun <ssse88@naver.com>
2017-11-28 00:12:40 -0800

    chatting main

--- app/chatting.c
+++ app/chatting.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
-#include "../include/chatClient.h"
 #include "../include/chatServer.h"
+#include "../include/chatClient.h"
 
 int main()
 {
-  printf("              * welcome to chat *               \n");
-  printf("┌──────────────────────────────────────────────┐\n");  
-  printf("│                                              │\n");  
-  printf("│             1. made a room                   │\n");  
-  printf("│             2. entry a room                  │\n");  
-  printf("│                                              │\n");  
-  printf("└──────────────────────────────────────────────┘\n");
 
- 
   int n;
-  scanf("%d",&n);
+
+   printf("              * welcome to chat *               \n");
+   printf("┌──────────────────────────────────────────────┐\n");
+   printf("│                                              │\n");
+   printf("│             1. made a room                   │\n");
+   printf("│             2. entry a room                  │\n");
+   printf("│                                              │\n");
+   printf("└──────────────────────────────────────────────┘\n");
+
+
+   scanf("%d",&n);
 
   while(1)
   {
      if(n==1)
      {
-        chatServer();
+        server();
      }
      else if(n==2)
      {
-        chatClient();
+        client();
      }
      else
      {
        scanf("%d",&n);
      }
+  }
 
+  return 0;
 }
